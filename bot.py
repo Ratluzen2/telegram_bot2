58a15dcf31c1ae5ace46a47c125bd3696d570f38
Ratluzen2 <zafer.s.2.693@gmail.com>
2025-02-21 11:49:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # إعداد المتغيرات العامة
-ADMIN_ID = 7655504656  # عدل الآيدي حسب المالك
+ADMIN_ID = "7655504656"  # عدل الآيدي حسب المالك
 TOKEN = "8138615524:AAEZGgBRMSzLxxC7F6NquT4dbmk5vA-2w4M"  # ضع توكن البوت الخاص بك هنا
 API_KEY = "8a94d8898e614971fde46ce1ca892202"  # ضع API KEY الخاص بك هنا
 API_URL = "https://kd1s.com/api/v2"  # تأكد من صحة رابط API
