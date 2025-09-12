740acc9038a8978302e3c7097ae93ded2dcc5f1c
Ratluzen2 <zafer.s.2.693@gmail.com>
2025-09-12 15:19:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ logger = logging.getLogger("TG_BOT")
 # =========================
 # الإعدادات (Environment)
 # =========================
-ADMIN_ID = int(os.getenv("ADMIN_ID", "7655504656"))   # مثال: 7655504656
+ADMIN_ID = int(os.getenv("ADMIN_ID", 7655504656))   # مثال: 7655504656
 TOKEN = os.getenv("TOKEN", "8138615524:AAFr6m5Z4_gY0k7pdg7teD9nM8ReDC-KQKU")  # مثال: "123456:AA...."
 API_KEY = os.getenv("API_KEY", "25a9ceb07be0d8b2ba88e70dcbe92e06")
 API_URL = os.getenv("API_URL", "https://kd1s.com/api/v2")
