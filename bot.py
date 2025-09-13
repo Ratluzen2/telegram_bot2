17e11ab93d9014458c86d5595e7cc1a1670f3488
Ratluzen2 <zafer.s.2.693@gmail.com>
2025-09-14 01:44:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,8 +47,8 @@ logger = logging.getLogger("TG_BOT")
 # =========================
 # الإعدادات (Environment)
 # =========================
-ADMIN_ID = int(os.getenv("8138615524:AAFr6m5Z4_gY0k7pdg7teD9nM8ReDC-KQKU"))
-TOKEN = os.getenv("TOKEN", "8138:dummy_token_change_me")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7655504656"))
+TOKEN = os.getenv("TOKEN", "8138615524:AAFr6m5Z4_gY0k7pdg7teD9nM8ReDC-KQKU")
 API_KEY = os.getenv("API_KEY", "25a9ceb07be0d8b2ba88e70dcbe92e06")
 API_URL = os.getenv("API_URL", "https://kd1s.com/api/v2")
 SUPPORT_CONTACT = os.getenv("SUPPORT_CONTACT", "@z396r")  # لدعم طرق الشحن الإضافية
